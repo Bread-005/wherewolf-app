9224a606765ddd237cb413e9c8990c055346ea92
jenspromess <jenspromess@web.de>
2026-04-23 18:48:08 +0200

    adjusted wiki-page styling

--- wiki.js
+++ wiki.js
@@ -1,25 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
-    // test data for now (later database roles)
-    const roles = [
-        {
-            name: "Werewolf",
-            team: "Werewolf",
-            abilityText: "See other werewolves. If alone, may view 1 center card"
-        },
-        {
-            name: "Seer",
-            team: "Villager",
-            abilityText: "Either view 1 player´s card or 2 center cards",
-        }
-    ];
-
-    displayWikiRole(roles[0]);
+    const roles = await fetch("./roles.json").then(res => res.json());
 
     for (const role of roles) {
         const option = new Option(role.name, JSON.stringify(role));
         document.getElementById("wiki-role-selection").add(option);
     }
+
+    displayWikiRole(roles[0]);
 
     document.getElementById("wiki-role-selection").addEventListener("change", (event) => {
         const role = JSON.parse(event.target.value);
@@ -27,8 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     function displayWikiRole(role) {
-        document.getElementById("wiki-role-name").textContent = role.name;
-        document.getElementById("wiki-role-team").textContent = role.team;
-        document.getElementById("wiki-role-ability-text").textContent = role.abilityText;
+        if (role.team === "Villager") {
+            document.getElementById("wiki-role-container").style.background = "green";
+            document.getElementById("wiki-role-team").className = "team-villager";
+            document.getElementById("wiki-role-container").style.borderTop = "8px solid #27ae60";
+        } else {
+            document.getElementById("wiki-role-container").style.background = "purple";
+            document.getElementById("wiki-role-team").className = "team-wolf";
+            document.getElementById("wiki-role-container").style.borderTop = "8px solid #c0392b";
+        }
+        document.getElementById("wiki-role-name").textContent = "Name: " + role.name;
+        document.getElementById("wiki-role-team").textContent = "Team: " + role.team;
+        document.getElementById("wiki-role-description").textContent = "Description: nothing here yet";
+        document.getElementById("wiki-role-night-action").textContent = "Night Action: " + (role.nightAction ? role.nightAction : "none");
+        document.getElementById("wiki-role-night-action-detailed").textContent = role.nightActionDetailed ? "Detailed: " + role.nightActionDetailed : "";
     }
 });
